9ba325b020a253776db6d7b340c0793ba3656455
maria liza <mariamartinez@pursuit.org>
2019-12-18 21:28:04 -0500

    add styling in css

--- index.js
+++ index.js
@@ -8,6 +8,67 @@ document.addEventListener("DOMContentLoaded", () => {
   let submitInput = document.querySelector("#submitInput")
   let movieInfo = document.querySelector("#movieInfo")
 
+  let h3 = document.createElement("h3")
+  movieInfo.appendChild(h3)
+  let dateP = document.createElement("p")
+  movieInfo.appendChild(dateP)
+  let descriptionP = document.createElement("p")
+  movieInfo.appendChild(descriptionP)
+
+  select.addEventListener("change", async e => {
+    h3.innerText = e.target.value
+    try {
+      let res = await axios.get("https://ghibliapi.herokuapp.com/films")
+      res.data.forEach(movie => {
+        if (e.target.value === movie.title) {
+          dateP.innerText = movie.release_date
+          descriptionP.innerText = movie.description
+        }
+      })
+    } catch (err) {
+      console.log(err)
+    }
+
+    let ul = document.createElement("ul")
+    form.appendChild(ul)
+    let li = document.createElement("li")
+    ul.appendChild(li)
+    let titleP = document.createElement("p")
+
+    form.addEventListener("submit", async e => {
+      // h2.innerText = `${movie.title}:`
+      e.preventDefault()
+      try {
+        let res = await axios.get("https://ghibliapi.herokuapp.com/films")
+        res.data.forEach(movie => {
+        //   if (e.target.value === movie.title) {
+            // titleP.innerText = res.data.movie.title
+            li.innerText = `${textInput.value}`
+            form.appendChild(titleP)
+        //   }
+        })
+      } catch (err) {
+        console.log(err)
+      }
+      // ul.removeChild(li)
+    })
+  })
+
+  // select.addEventListener("change", async e => {
+  //     h3.innerText = e.target.value
+  //     try {
+  //       let res = await axios.get("https://ghibliapi.herokuapp.com/films")
+  //       res.data.forEach(movie => {
+  //         if (e.target.value === movie.title) {
+  //           dateP.innerText = movie.release_date
+  //           descriptionP.innerText = movie.description
+  //         }
+  //       })
+  //     } catch (err) {
+  //       console.log(err)
+  //     }
+  //   })
+
   const populateSelect = async () => {
     try {
       let res = await axios.get("https://ghibliapi.herokuapp.com/films")
@@ -21,41 +82,40 @@ document.addEventListener("DOMContentLoaded", () => {
       console.log(err)
     }
 
-    let h3 = document.createElement("h3")
-    movieInfo.appendChild(h3)
-    let dateP = document.createElement("p")
-    movieInfo.appendChild(dateP)
-    let descriptionP = document.createElement("p")
-    movieInfo.appendChild(descriptionP)
+    // let h3 = document.createElement("h3")
+    // movieInfo.appendChild(h3)
+    // let dateP = document.createElement("p")
+    // movieInfo.appendChild(dateP)
+    // let descriptionP = document.createElement("p")
+    // movieInfo.appendChild(descriptionP)
 
-    select.addEventListener("change", async e => {
-      h3.innerText = e.target.value
-      try {
-        let res = await axios.get("https://ghibliapi.herokuapp.com/films")
-        res.data.forEach(movie => {
-          if (e.target.value === movie.title) {
-            dateP.innerText = movie.release_date
-            descriptionP.innerText = movie.description
-          }
-        })
-      } catch (err) {
-        console.log(err)
-      }
-    })
+    // select.addEventListener("change", async e => {
+    //   h3.innerText = e.target.value
+    //   try {
+    //     let res = await axios.get("https://ghibliapi.herokuapp.com/films")
+    //     res.data.forEach(movie => {
+    //       if (e.target.value === movie.title) {
+    //         dateP.innerText = movie.release_date
+    //         descriptionP.innerText = movie.description
+    //       }
+    //     })
+    //   } catch (err) {
+    //     console.log(err)
+    //   }
+    // })
 
     // let reviewDiv = document.createElement("div")
     // body.appendChild(reviewDiv)
-    
-    form.addEventListener("submit", async e => {
-        e.preventDefault();
-        let ul = document.createElement("ul")
-        form.appendChild(ul)
-        let li = document.createElement("li")
-        ul.appendChild(li)
-        if (e.target.value === movie.title)
-        li.innerText = movie.title
-    })
-    
-}
-    populateSelect()
+
+    // form.addEventListener("submit", async e => {
+    //     e.preventDefault();
+    //     let ul = document.createElement("ul")
+    //     form.appendChild(ul)
+    //     let li = document.createElement("li")
+    //     ul.appendChild(li)
+    //     if (e.target.value === movie.title)
+    //     li.innerText = movie.title
+    // })
+  }
+  populateSelect()
 })
