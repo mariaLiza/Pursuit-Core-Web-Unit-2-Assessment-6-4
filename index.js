ba781424a80d5143d82b573f2b88f400a0390dd7
maria liza <mariamartinez@pursuit.org>
2019-12-18 21:39:22 -0500

    remove commented out code

--- index.js
+++ index.js
@@ -36,38 +36,19 @@ document.addEventListener("DOMContentLoaded", () => {
     let titleP = document.createElement("p")
 
     form.addEventListener("submit", async e => {
-      // h2.innerText = `${movie.title}:`
       e.preventDefault()
       try {
         let res = await axios.get("https://ghibliapi.herokuapp.com/films")
         res.data.forEach(movie => {
-        //   if (e.target.value === movie.title) {
-            // titleP.innerText = res.data.movie.title
-            li.innerText = `${textInput.value}`
-            form.appendChild(titleP)
-        //   }
+          select.value = movie[i].title
         })
+
+        li.innerText = `${textInput.value}`
       } catch (err) {
         console.log(err)
       }
-      // ul.removeChild(li)
     })
   })
-
-  // select.addEventListener("change", async e => {
-  //     h3.innerText = e.target.value
-  //     try {
-  //       let res = await axios.get("https://ghibliapi.herokuapp.com/films")
-  //       res.data.forEach(movie => {
-  //         if (e.target.value === movie.title) {
-  //           dateP.innerText = movie.release_date
-  //           descriptionP.innerText = movie.description
-  //         }
-  //       })
-  //     } catch (err) {
-  //       console.log(err)
-  //     }
-  //   })
 
   const populateSelect = async () => {
     try {
@@ -81,41 +62,6 @@ document.addEventListener("DOMContentLoaded", () => {
     } catch (err) {
       console.log(err)
     }
-
-    // let h3 = document.createElement("h3")
-    // movieInfo.appendChild(h3)
-    // let dateP = document.createElement("p")
-    // movieInfo.appendChild(dateP)
-    // let descriptionP = document.createElement("p")
-    // movieInfo.appendChild(descriptionP)
-
-    // select.addEventListener("change", async e => {
-    //   h3.innerText = e.target.value
-    //   try {
-    //     let res = await axios.get("https://ghibliapi.herokuapp.com/films")
-    //     res.data.forEach(movie => {
-    //       if (e.target.value === movie.title) {
-    //         dateP.innerText = movie.release_date
-    //         descriptionP.innerText = movie.description
-    //       }
-    //     })
-    //   } catch (err) {
-    //     console.log(err)
-    //   }
-    // })
-
-    // let reviewDiv = document.createElement("div")
-    // body.appendChild(reviewDiv)
-
-    // form.addEventListener("submit", async e => {
-    //     e.preventDefault();
-    //     let ul = document.createElement("ul")
-    //     form.appendChild(ul)
-    //     let li = document.createElement("li")
-    //     ul.appendChild(li)
-    //     if (e.target.value === movie.title)
-    //     li.innerText = movie.title
-    // })
   }
   populateSelect()
 })
