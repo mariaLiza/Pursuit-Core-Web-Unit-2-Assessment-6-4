32671b821056810d4e5f765225675a85d9332df3
maria liza <mariamartinez@pursuit.org>
2019-12-18 20:10:00 -0500

    add some css to img and background

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ document.addEventListener("DOMContentLoaded", () => {
   let option = document.querySelector("option")
   let textInput = document.querySelector("#textInput")
   let submitInput = document.querySelector("#submitInput")
+  let movieInfo = document.querySelector("#movieInfo")
 
   const populateSelect = async () => {
     try {
@@ -19,7 +20,42 @@ document.addEventListener("DOMContentLoaded", () => {
     } catch (err) {
       console.log(err)
     }
-  }
 
-  populateSelect()
+    let h3 = document.createElement("h3")
+    movieInfo.appendChild(h3)
+    let dateP = document.createElement("p")
+    movieInfo.appendChild(dateP)
+    let descriptionP = document.createElement("p")
+    movieInfo.appendChild(descriptionP)
+
+    select.addEventListener("change", async e => {
+      h3.innerText = e.target.value
+      try {
+        let res = await axios.get("https://ghibliapi.herokuapp.com/films")
+        res.data.forEach(movie => {
+          if (e.target.value === movie.title) {
+            dateP.innerText = movie.release_date
+            descriptionP.innerText = movie.description
+          }
+        })
+      } catch (err) {
+        console.log(err)
+      }
+    })
+
+    // let reviewDiv = document.createElement("div")
+    // body.appendChild(reviewDiv)
+    
+    form.addEventListener("submit", async e => {
+        e.preventDefault();
+        let ul = document.createElement("ul")
+        form.appendChild(ul)
+        let li = document.createElement("li")
+        ul.appendChild(li)
+        if (e.target.value === movie.title)
+        li.innerText = movie.title
+    })
+    
+}
+    populateSelect()
 })
